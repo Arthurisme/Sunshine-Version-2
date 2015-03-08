c15fb1c24d350eabd9212d14f5fdf63d13cdac9e
Arthurisme <nebula.trek@gmail.com>
2015-03-07 21:11:54 -0500

    5.07-Coding-task-for-Details-screen

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -22,7 +22,8 @@ import android.preference.PreferenceManager;
 
 import java.text.DateFormat;
 import java.util.Date;
-import  java.util.Calendar;
+import java.util.Calendar;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -48,14 +49,21 @@ public class Utility {
                 .equals(context.getString(R.string.pref_units_metric));
     }
 
-    static String formatTemperature(double temperature, boolean isMetric) {
+    static String formatTemperature(Context context, double temperature, boolean isMetric) {
         double temp;
-        if ( !isMetric ) {
-            temp = 9*temperature/5+32;
+        if (!isMetric) {
+            temp = 9 * temperature / 5 + 32;
         } else {
             temp = temperature;
         }
-        return String.format("%.0f", temp);
+        int formatId_t = R.string.format_tempature;
+        // return String.format("%.0f"+"°C", temp);
+        return
+                //String.format
+                (context.getString(
+                        formatId_t,
+                        (int) temp
+                ));
     }
 
     static String formatDate(long dateInMillis) {
@@ -65,21 +73,21 @@ public class Utility {
 
     static String getDayofWeek(long dateInMillis) {
 
-        java.util.Calendar c = java.util.Calendar.getInstance() ;
+        java.util.Calendar c = java.util.Calendar.getInstance();
 
         c.setTimeInMillis(dateInMillis);
 
-        int dayofweek=c.get(Calendar.DAY_OF_WEEK);
+        int dayofweek = c.get(Calendar.DAY_OF_WEEK);
 
 
-        return dayofweek+"thw";
+        return dayofweek + "thw";
     }
 
     /**
      * Helper method to convert the database representation of the date into something to display
      * to users.  As classy and polished a user experience as "20140102" is, we can do better.
      *
-     * @param context Context to use for resource localization
+     * @param context      Context to use for resource localization
      * @param dateInMillis The date in milliseconds
      * @return a user-friendly representation of the date.
      */
@@ -102,11 +110,13 @@ public class Utility {
             String today = context.getString(R.string.today);
             int formatId = R.string.format_full_friendly_date;
             Log.d("2398 a", "2398 a,xliff: " + formatId);
-            return String.format(context.getString(
-                    formatId,
-                    today,
-                    getFormattedMonthDay(context, dateInMillis)));
-        } else if ( julianDay < currentJulianDay + 7 ) {
+            return
+                    //String.format
+                    (context.getString(
+                            formatId,
+                            today,
+                            getFormattedMonthDay(context, dateInMillis)));
+        } else if (julianDay < currentJulianDay + 7) {
             // If the input date is less than a week in the future, just return the day name.
             return getDayName(context, dateInMillis);
         } else {
@@ -126,9 +136,13 @@ public class Utility {
         int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
         if (julianDay == currentJulianDay) {
             return context.getString(R.string.today);
-        } else if ( julianDay == currentJulianDay +1 ) {
+        } else if (julianDay == currentJulianDay + 1) {
             return context.getString(R.string.tomorrow);
-        } else {
+        }
+     else if (julianDay == currentJulianDay + 2) {
+        return  ("The Day After Tomorrow");
+    }
+    else {
             Time time = new Time();
             time.setToNow();
             // Otherwise, the format is just the day of the week (e.g "Wednesday".
@@ -139,18 +153,53 @@ public class Utility {
 
     /**
      * Converts db date format to the format "Month day", e.g "June 24".
+     *
      * @param context Context to use for resource localization
      * @param dateInMillis The db formatted date string, expected to be of the form specified
-     *                in Utility.DATE_FORMAT
+     * in Utility.DATE_FORMAT
      * @return The day in the form of a string formatted "December 6"
      */
     public static final String DATE_FORMAT = "yyyyMMdd";
-    public static String getFormattedMonthDay(Context context, long dateInMillis ) {
+
+    public static String getFormattedMonthDay(Context context, long dateInMillis) {
         Time time = new Time();
         time.setToNow();
         SimpleDateFormat dbDateFormat = new SimpleDateFormat(Utility.DATE_FORMAT);
         SimpleDateFormat monthDayFormat = new SimpleDateFormat("MMMM dd");
         String monthDayString = monthDayFormat.format(dateInMillis);
         return monthDayString;
+    }
+
+    public static String getFormattedWind(Context context, float windSpeed, float degrees) {
+        int windFormat;
+        if (Utility.isMetric(context)) {
+            windFormat = R.string.format_wind_kmh;
+        } else {
+            windFormat = R.string.format_wind_mph;
+            windSpeed = .621371192237334f * windSpeed;
+        }
+
+        // From wind direction in degrees, determine compass direction as a string (e.g NW)
+        // You know what's fun, writing really long if/else statements with tons of possible
+        // conditions.  Seriously, try it!
+        String direction = "Unknown";
+        if (degrees >= 337.5 || degrees < 22.5) {
+            direction = "N";
+        } else if (degrees >= 22.5 && degrees < 67.5) {
+            direction = "NE";
+        } else if (degrees >= 67.5 && degrees < 112.5) {
+            direction = "E";
+        } else if (degrees >= 112.5 && degrees < 157.5) {
+            direction = "SE";
+        } else if (degrees >= 157.5 && degrees < 202.5) {
+            direction = "S";
+        } else if (degrees >= 202.5 && degrees < 247.5) {
+            direction = "SW";
+        } else if (degrees >= 247.5 && degrees < 292.5) {
+            direction = "W";
+        } else if (degrees >= 292.5 && degrees < 337.5) {
+            direction = "NW";
+        }
+        return String.format(context.getString(windFormat), windSpeed, direction);
     }
 }
